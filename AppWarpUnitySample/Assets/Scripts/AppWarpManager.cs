f053e677b0e425d1537b43fb5f326e46bfb39d77
RajeevRShephertz <rajeev.ranjan@shephertz.co.in>
2015-12-22 15:01:40 +0530

    AppWarpUnitySample

---
+++ AppWarpUnitySample/Assets/Scripts/AppWarpManager.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using com.shephertz.app42.gaming.multiplayer.client;
+using com.shephertz.app42.gaming.multiplayer.client.events;
+using com.shephertz.app42.gaming.multiplayer.client.listener;
+using com.shephertz.app42.gaming.multiplayer.client.command;
+using com.shephertz.app42.gaming.multiplayer.client.message;
+using com.shephertz.app42.gaming.multiplayer.client.transformer;
+using AssemblyCSharp;
+using UnityEngine.UI;
+
+public class AppWarpManager : MonoBehaviour {
+
+	public string apiKey = "Your API Key";
+	public string secretKey = "Your Secret Key";
+	public InputField username;
+	public InputField roomName;
+	public InputField maxUsers;
+	public InputField roomID;
+	public InputField chatInput;
+	public InputField charUsername;
+
+	MyListener listen;
+	Rect myRect;
+//	IEnumerator e;
+	// Use this for initialization
+	void Start () {
+		WarpClient.initialize(apiKey,secretKey);
+		listen = GetComponent<MyListener>();
+		WarpClient.GetInstance().AddConnectionRequestListener(listen);
+		WarpClient.GetInstance().AddChatRequestListener(listen);
+		WarpClient.GetInstance().AddLobbyRequestListener(listen);
+		WarpClient.GetInstance().AddNotificationListener(listen);
+		WarpClient.GetInstance().AddRoomRequestListener(listen);
+		WarpClient.GetInstance().AddUpdateRequestListener(listen);
+		WarpClient.GetInstance().AddZoneRequestListener(listen);
+
+		myRect = new Rect (10, 350, 500, 300);
+//		StartCoroutine ("SomeFunc");
+	}
+
+//	IEnumerator SomeFunc(){
+//		e = execute ("");
+//		while (e != null && e.MoveNext()) {
+//			Debug.Log("asd");
+//			yield return e.Current;
+//		}
+//	}
+//
+//	IEnumerator execute (string es){
+//		yield return new WaitForSeconds (10);
+//		Debug.Log ("Done");
+//	}
+
+	//Update
+	void Update(){
+		WarpClient.GetInstance().Update();
+//		if (Input.GetKeyDown (KeyCode.W)) {
+//			e = null;
+//		}
+	}
+
+	//OnGUI
+	void OnGUI()
+	{
+		GUI.contentColor = Color.black;
+		GUI.Label(myRect, listen.getDebug());
+	}
+
+	//Application Quit
+	void OnApplicationQuit()
+	{
+		WarpClient.GetInstance().Disconnect();
+	}
+
+	// Connect
+	public void Connect () {
+		Debug.Log ("Connect Called......"+System.DateTime.Now);
+		WarpClient.GetInstance ().Connect(username.text);
+	}
+	// DisConnect
+	public void Disconnect () {
+		WarpClient.GetInstance().Disconnect();
+	}
+	// GetAllRooms
+	public void GetAllRoom () {
+		WarpClient.GetInstance().GetAllRooms();
+	}
+	// Connect
+	public void CreateRoom () {
+		if (WarpClient.GetInstance ().GetConnectionState ()== WarpConnectionState.CONNECTED) {
+			Debug.Log("Creating Room..........");
+			WarpClient.GetInstance ().CreateRoom (roomName.text, username.text, int.Parse (maxUsers.text), null);
+		} else {
+			Debug.Log("Not Connected........Try Again");
+		}
+	}
+	// GetUserInfo
+	public void GetUserInfo () {
+		WarpClient.GetInstance().GetLiveUserInfo(username.text);
+	}
+	// JoinRoom
+	public void JoinRoom () {
+		WarpClient.GetInstance().JoinRoom(roomID.text);
+	}
+	// GetRoomInfo
+	public void GetRoomInfo () {
+		WarpClient.GetInstance().GetLiveRoomInfo(roomID.text);
+	}
+	// SubscribeRoom
+	public void SubscribeRoom () {
+		WarpClient.GetInstance().SubscribeRoom(roomID.text);
+	}
+	// LeaveRoom
+	public void LeaveRoom () {
+		WarpClient.GetInstance().LeaveRoom(roomID.text);
+	}
+	// UnSubscribeRoom
+	public void UnSubscribeRoom () {
+		WarpClient.GetInstance().UnsubscribeRoom(roomID.text);
+	}
+	// GetLobbyInfo
+	public void GetLobbyInfo () {
+		WarpClient.GetInstance().GetLiveLobbyInfo();
+	}
+	// JoinLobby
+	public void JoinLobby () {
+		//WarpClient.GetInstance().JoinLobby();
+		//var array = Encoding.ASCII.GetBytes(new string("how are you", 100));
+		byte[] smallArray = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+		WarpClient.GetInstance ().SendUpdatePeers (smallArray);
+	}
+	// LeaveLobby
+	public void LeaveLobby () {
+		//WarpClient.GetInstance().LeaveLobby();
+//		for(int i=0, i<1000, i++){
+//			WarpClient.GetInstance().SendChat("Hi, how are you?");
+//		}
+
+	}
+	// GetOnlineUsers
+	public void GetOnlineUsers () {
+		WarpClient.GetInstance().GetOnlineUsers();
+	}
+	// SendChat
+	public void SendChat () {
+		WarpClient.GetInstance().SendChat(chatInput.text);
+	}
+	// SendPrivateChat
+	public void SendPrivateChat () {
+		WarpClient.GetInstance().sendPrivateChat(charUsername.text, chatInput.text);
+	}
+	// DeleteRoom
+	public void DeleteRoom () {
+		WarpClient.GetInstance().DeleteRoom(roomID.text);
+	}
+}
---
+++ AppWarpUnitySample/Assets/Scripts/MyListener.cs
@@ -0,0 +1,252 @@
+﻿using UnityEngine;
+
+using com.shephertz.app42.gaming.multiplayer.client;
+using com.shephertz.app42.gaming.multiplayer.client.events;
+using com.shephertz.app42.gaming.multiplayer.client.listener;
+using com.shephertz.app42.gaming.multiplayer.client.command;
+using com.shephertz.app42.gaming.multiplayer.client.message;
+using com.shephertz.app42.gaming.multiplayer.client.transformer;
+using System;
+using System.Collections.Generic;
+
+namespace AssemblyCSharp
+{
+	public class MyListener : MonoBehaviour, ConnectionRequestListener, ZoneRequestListener, ChatRequestListener, LobbyRequestListener, NotifyListener, RoomRequestListener, UpdateRequestListener {
+
+		string debug = "";
+
+		private void Log(string msg)
+		{
+			debug = msg + "\n" + debug;
+			Debug.Log (debug);
+		}
+
+		public string getDebug()
+		{
+			return debug;
+		}
+
+//		public void onInvokeZoneRPCDone (RPCEvent rpcEvent)
+//		{
+//
+//		}
+//
+//		public void onInvokeRoomRPCDone (RPCEvent rpcEvent)
+//		{
+//
+//		}
+
+		public void onConnectDone(ConnectEvent eventObj){
+			Debug.Log ("Time:"+System.DateTime.Now);
+			Log ("onConnectDone : " + eventObj.getResult());
+		}
+
+		public void onDisconnectDone (ConnectEvent eventObj){
+			Log ("onDisConnectDone : " + eventObj.getResult());
+		}
+
+		public void onJoinRoomDone (RoomEvent eventObj){
+			Log ("onJoinRoomDone : " + eventObj.getResult());
+		}
+
+		public void onGetLiveRoomInfoDone (LiveRoomInfoEvent eventObj){
+			Log ("onGetLiveRoomInfoDone : " + eventObj.getResult()  + ", RoomName : " + eventObj.getData().getName() + ", RoomID : " + eventObj.getData().getId() + ", RoomOwner : " + eventObj.getData().getRoomOwner() + ", MaxUsers : " + eventObj.getData().getMaxUsers());
+		}
+
+		public void onInitUDPDone (byte resultCode){
+		}
+
+		public void onSendChatDone (byte result){
+			Log ("onSendChatDone : " + result);
+		}
+
+		public void onSendPrivateChatDone (byte result){
+			Log ("onSendPrivateChatDone : " + result);
+		}
+
+		public void onGetLiveLobbyInfoDone (LiveRoomInfoEvent eventObj){
+			Log ("onGetLiveLobbyInfoDone : " + eventObj.getResult()  + ", RoomName : " + eventObj.getData().getName() + ", RoomID : " + eventObj.getData().getId());
+		}
+		
+		public void onJoinLobbyDone (LobbyEvent eventObj){
+			Log ("onJoinLobbyDone : " + eventObj.getResult()  + ", RoomName : " + eventObj.getInfo().getName() + ", RoomID : " + eventObj.getInfo().getId());
+		}
+		
+		public void onLeaveLobbyDone (LobbyEvent eventObj){
+			Log ("onLeaveLobbyDone : " + eventObj.getResult()  + ", RoomName : " + eventObj.getInfo().getName() + ", RoomID : " + eventObj.getInfo().getId());
+		}
+		
+		public void onSubscribeLobbyDone (LobbyEvent eventObj){
+
+		}
+		
+		public void onUnSubscribeLobbyDone (LobbyEvent eventObj){
+
+		}
+
+		public void onCreateRoomDone (RoomEvent eventObj){
+			Log ("onCreateRoomDone : " + eventObj.getResult() + ", RoomName : " + eventObj.getData().getName() + ", RoomID : " + eventObj.getData().getId());
+		}
+		
+		public void onDeleteRoomDone (RoomEvent eventObj){
+			Log ("onDeleteRoomDone : " + eventObj.getResult() + ", RoomName : " + eventObj.getData().getName() + ", RoomID : " + eventObj.getData().getId());
+		}
+
+		public void onGetAllRoomsDone (AllRoomsEvent eventObj){
+			Log ("onGetAllRoomsDone : " + eventObj.getResult());
+			string[] rooms = eventObj.getRoomIds ();
+			if (rooms != null && rooms.Length != 0) {
+					foreach (string room in rooms) {
+						Log ("room " + room);
+				}
+			}
+		}
+
+		public void onGetLiveUserInfoDone (LiveUserInfoEvent eventObj){
+			Log ("onGetLiveUserInfoDone : " + eventObj.getResult() + ", Name : " + eventObj.getName() + ", IsInLobby : " + eventObj.isLocationLobby());
+		}
+		
+		public void onGetMatchedRoomsDone (MatchedRoomsEvent matchedRoomsEvent){
+			Log ("onGetMatchedRoomsDone : " + matchedRoomsEvent.getResult());
+		}
+		
+		public void onGetOnlineUsersDone (AllUsersEvent eventObj){
+			Log ("onGetOnlineUsersDone : " + eventObj.getResult());
+			string[] usernames = eventObj.getUserNames ();
+			if (usernames != null && usernames.Length != 0) {
+				foreach (string username in usernames) {
+					Log ("username " + username);
+				}
+			}
+		}
+		
+		public void onSetCustomUserDataDone (LiveUserInfoEvent eventObj){
+			Log ("onSetCustomUserDataDone : " + eventObj.getResult());
+		}
+
+		//NotifyListener
+		public void onRoomCreated (RoomData eventObj)
+		{
+			Log ("onRoomCreated");
+		}
+		
+		public void onRoomDestroyed (RoomData eventObj)
+		{
+			Log ("onRoomDestroyed");
+		}
+		
+		public void onUserLeftRoom (RoomData eventObj, string username)
+		{
+			Log ("onUserLeftRoom : " + username);
+		}
+		
+		public void onUserJoinedRoom (RoomData eventObj, string username)
+		{
+			Log ("onUserJoinedRoom : " + username);
+		}
+		
+		public void onUserLeftLobby (LobbyData eventObj, string username)
+		{
+			Log ("onUserLeftLobby : " + username);
+		}
+		
+		public void onUserJoinedLobby (LobbyData eventObj, string username)
+		{
+			Log ("onUserJoinedLobby : " + username);
+		}
+		
+		public void onUserChangeRoomProperty(RoomData roomData, string sender, Dictionary<string, object> properties, Dictionary<string, string> lockedPropertiesTable)
+		{
+			Log ("onUserChangeRoomProperty : " + sender);
+		}
+		
+		public void onPrivateChatReceived(string sender, string message)
+		{
+			Log ("onPrivateChatReceived Successfull Sender : " + sender + ", Message : " + message);
+		}
+		
+		public void onMoveCompleted(MoveEvent move)
+		{
+			Log ("onMoveCompleted by : " + move.getSender());
+		}
+
+		public void onUserPaused(String locid, Boolean isLobby, String username)
+		{
+		}
+		
+		public void onUserResumed(String locid, Boolean isLobby, String username)
+		{
+		}
+		
+		public void onGameStarted(string sender, string roomId, string nextTurn)
+		{
+		}
+		
+		public void onGameStopped(string sender, string roomId)
+		{
+		}
+		
+		public void onPrivateUpdateReceived(String sender, byte[] update, bool fromUdp)
+		{
+			
+		}
+
+		public void onNextTurnRequest(String lastTurn)
+		{
+			
+		}
+
+		public void onChatReceived (ChatEvent eventObj){
+			Log ("onChatReceived Successfull Sender : " + eventObj.getSender() + ", Message : " + eventObj.getMessage());
+		}
+
+		public void onUpdatePeersReceived (UpdateEvent eventObj)
+		{
+			Debug.Log ("onUpdatePeersReceived.....Called");
+		}
+
+		public void onLockPropertiesDone(byte result)
+		{
+			Log ("onLockPropertiesDone : " + result);
+		}
+		
+		public void onUnlockPropertiesDone(byte result)
+		{
+			Log ("onUnlockPropertiesDone : " + result);
+		}
+
+		public void onLeaveRoomDone (RoomEvent eventObj)
+		{
+			Log ("onLeaveRoomDone : " + eventObj.getResult() + ", RoomName : " + eventObj.getData().getName() + ", RoomID : " + eventObj.getData().getId());
+		}
+		
+		public void onSetCustomRoomDataDone (LiveRoomInfoEvent eventObj)
+		{
+			Log ("onSetCustomRoomDataDone : " + eventObj.getResult());
+		}
+
+		//RoomRequestListener
+		public void onSubscribeRoomDone (RoomEvent eventObj)
+		{			
+			Log ("onSubscribeRoomDone : " + eventObj.getResult() + ", RoomName : " + eventObj.getData().getName() + ", RoomID : " + eventObj.getData().getId());
+		}
+		
+		public void onUnSubscribeRoomDone (RoomEvent eventObj)
+		{
+			Log ("onUnSubscribeRoomDone : " + eventObj.getResult() + ", RoomName : " + eventObj.getData().getName() + ", RoomID : " + eventObj.getData().getId());
+		}
+
+		public void onUpdatePropertyDone(LiveRoomInfoEvent eventObj){
+		}
+
+		//UpdateRequestListener
+		public void onSendUpdateDone (byte result)
+		{
+			Debug.Log ("Yo, Got the result onSendUpdateDone " + result);
+		}
+		
+		public void onSendPrivateUpdateDone(byte result)
+		{
+		}
+	}
+}
